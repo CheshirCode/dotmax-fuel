420d6051b5f3497185e6152c3f7ad088f65711bd
CheshireCode <50378606+CheshirCode@users.noreply.github.com>
2022-05-29 21:30:50 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-'use strict'
+"use strict "
+const mainForm = document.forms.formFuel;
+console.log(mainForm);
+const mainFormTruck = mainForm.elements.truck;
+console.log(mainFormTruck);
+const mainFormRefrigerator = mainForm.elements.refrigerator;
+console.log(mainFormRefrigerator);
+const mainFormHeatingEngine = mainForm.elements.heatingEngine;
+console.log(mainFormHeatingEngine);
+
+const truckValue = mainFormTruck.value = 100.350;
+console.log(truckValue);
+if (mainFormTruck.value < 0) {
+  alert("Неверное значение");
+  mainFormTruck.value = "";
+} else {
+  mainFormTruck.value;
+}
