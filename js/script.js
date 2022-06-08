310f27a9c852cc6035c23d74086e25d1b00f997b
CheshireCode <50378606+CheshirCode@users.noreply.github.com>
2022-06-08 20:38:28 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,11 +8,21 @@ console.log(mainFormRefrigerator);
 const mainFormHeatingEngine = mainForm.elements.heatingEngine;
 console.log(mainFormHeatingEngine);
 
-const truckValue = mainFormTruck.value = 100.350;
-console.log(truckValue);
-if (mainFormTruck.value < 0) {
-  alert("Неверное значение");
-  mainFormTruck.value = "";
-} else {
-  mainFormTruck.value;
-}
+// const truckValue = mainFormTruck.value = -100.350;
+// console.log(truckValue);
+// if (mainFormTruck.value < 0) {
+//   alert("Из своей канистры заправил? Молодец!");
+//   mainFormTruck.value = "";
+// } else {
+//   mainFormTruck.value;
+// }
+mainFormTruck.addEventListener("change", function (e) {
+  console.log(parseFloat(mainFormTruck.value));
+});
+mainFormRefrigerator.addEventListener("change", function (e) {
+  console.log(parseFloat(mainFormRefrigerator.value));
+});
+mainFormHeatingEngine.addEventListener("change", function (e) {
+  console.log(parseFloat(mainFormHeatingEngine.value));
+});
+console.log(parseFloat(mainFormHeatingEngine.value));
